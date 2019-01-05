af5be053f701f12379fa22e81a8d7e7ca5b279da
wudidatao <82302925@qq.com>
2019-01-05 16:09:09 +0800

    Update grafana_create_docker.sh

--- grafana_create_docker.sh
+++ grafana_create_docker.sh
@@ -6,6 +6,6 @@ docker run -d -p $grafana_port:$grafana_port \
 -v $grafana_data/grafana:/var/lib/grafana/ \
 -v $grafana_conf/grafana:/etc/grafana/ \
 -v /etc/localtime:/etc/localtime \
-#-e "GF_SECURITY_ADMIN_PASSWORD=$grafana_admin_password" \
+-e "GF_SECURITY_ADMIN_PASSWORD=$grafana_admin_password" \
 -e "GF_INSTALL_PLUGINS=alexanderzobnin-zabbix-app" \
 --restart=yes --restart=on-failure:3 --name $container_name-$grafana_description -h $hostname grafana/grafana:$version
